175827f85d4b3ff8fbc6a46d869d7ba9b2693747
Leonardo Criollo <criollo_banneret@tutanota.com>
2023-11-27 10:49:10 -0600

    Añadir formulario para ingresar dirección.

---
+++ src/components/AddressForm.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Form.css";
+
+export default function AddressForm() {
+    const [fullName, setFullName] = useState("");
+    const [streetAddress, setStreetAddress] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
+    const [zipCode, setZipCode] = useState("");
+
+    const handleFullNameChange = (event) => {
+        setFullName(event.target.value);
+    };
+
+    const handleStreetAddressChange = (event) => {
+        setStreetAddress(event.target.value);
+    };
+
+    const handleCityChange = (event) => {
+        setCity(event.target.value);
+    };
+
+    const handleStateChange = (event) => {
+        setState(event.target.value);
+    };
+
+    const handleZipCodeChange = (event) => {
+        setZipCode(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(fullName, streetAddress, city, state, zipCode);
+    };
+
+    return (
+        <form>
+            <div className="mb-3">
+                <label htmlFor="fullName" className="form-label">
+                    Nombre Completo:
+                </label>
+                <input
+                    type="text"
+                    id="fullName"
+                    value={fullName}
+                    aria-describedby="fullNameHelp"
+                    className="form-control"
+                    onChange={handleFullNameChange}
+                />
+                <small id="fullNameHelp" className="form-text text-muted">
+                    Por favor ingrese su nombre completo.
+                </small>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="streetAddress" className="form-label">
+                    Dirección:
+                </label>
+                <input
+                    type="text"
+                    id="streetAddress"
+                    value={streetAddress}
+                    aria-describedby="streetAddressHelp"
+                    className="form-control"
+                    onChange={handleStreetAddressChange}
+                />
+                <small id="streetAddressHelp" className="form-text text-muted">
+                    Dirección de la calle y número.
+                </small>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="city" className="form-label">
+                    Ciudad:
+                </label>
+                <input
+                    type="text"
+                    id="city"
+                    value={city}
+                    aria-describedby="cityHelp"
+                    className="form-control"
+                    onChange={handleCityChange}
+                />
+                <small id="cityHelp" className="form-text text-muted">
+                    Ciudad donde vive.
+                </small>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="state" className="form-label">
+                    Estado:
+                </label>
+                <input
+                    type="text"
+                    id="state"
+                    value={state}
+                    aria-describedby="stateHelp"
+                    className="form-control"
+                    onChange={handleStateChange}
+                />
+                <small id="stateHelp" className="form-text text-muted">
+                    Estado donde vive.
+                </small>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="zipCode" className="form-label">
+                    Código Postal:
+                </label>
+                <input
+                    type="text"
+                    id="zipCode"
+                    value={zipCode}
+                    aria-describedby="zipCodeHelp"
+                    className="form-control"
+                    onChange={handleZipCodeChange}
+                />
+                <small id="zipCodeHelp" className="form-text text-muted">
+                    Código postal de su vivienda.
+                </small>
+            </div>
+            <div className="d-grid gap-2">
+                <button
+                    type="submit"
+                    className="btn btn-mb-3 btn-primary"
+                    onClick={handleSubmit}
+                >
+                    Guardar
+                </button>
+            </div>
+        </form>
+    );
+}
--- src/components/CreditCardForm.js
+++ src/components/CreditCardForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./CreditCardForm.css";
+import "./Form.css";
 import submitPayment from "../services/submitPayment";
 
 export default function CreditCardForm() {
