a4c2727a9d7a6239f791091c70833620d1587872
MangoFizz <ccs81.mr@gmail.com>
2024-01-03 04:39:38 +0000

    Admin sidebar sign out button

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { CDBSidebar, CDBSidebarContent, CDBSidebarFooter, CDBSidebarHeader, CDBSidebarMenu, CDBSidebarMenuItem } from "cdbreact";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { strings } from "../localization";
+import { getLoggedUser, getLoggedUserName, isUserLogged, setLoggedUser } from '../helpers/loggedUser';
+import { Button } from "react-bootstrap";
 
 const AdminSidebar = () => {
+    const navigate = useNavigate();
+
+    const handleSignOutButtonClick = () => {
+        setLoggedUser(null);
+        navigate("/");
+    }
+
     return (
         <CDBSidebar className="sidebar-fixed vh-100" backgroundColor="#fff" textColor="#000">
             <CDBSidebarHeader>
                 <div className="d-flex align-items-center">
-                    <img src={'images/logo.png'} alt="" style={{ width: '30px' }}/>
+                    <img src={'/images/logo.png'} alt="" style={{ width: '30px' }}/>
                     <h6 style={{ marginBottom: "0", marginLeft: "0.4rem" }}>Abarrotes Cuevas</h6>
                 </div>
             </CDBSidebarHeader>
@@ -32,7 +41,12 @@ const AdminSidebar = () => {
                 </CDBSidebarMenu>
             </CDBSidebarContent>
             <CDBSidebarFooter style={{ textAlign: "center" }}>
-                <div style={{ padding: "20px 5px" }}>2023</div>
+                <Button variant="outline-danger" size="md" className="mb-3" onClick={handleSignOutButtonClick}>
+                    <div className="d-flex align-items-center justify-content-center">
+                        <i class="bi bi-box-arrow-left" style={{ marginRight: "0.4rem" }}></i>
+                        {strings.adminSidebar.signOut}
+                    </div>
+                </Button>
             </CDBSidebarFooter>
         </CDBSidebar>
     );
